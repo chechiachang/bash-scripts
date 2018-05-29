0995f9e44255dcf3db888d9ccd1ebb900a7ec425
David Chang <chechiachang@gmail.com>
2018-05-30 03:16:27 +0800

    Add x32 arch support

--- nvidia-driver.sh
+++ nvidia-driver.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Install nvidia driver on Ubuntu
 
+# TODO nvidia-driver -A
+# Add terminal install
+
 nvidia::driver::install(){
 
   version="390.59"
@@ -10,9 +13,15 @@ nvidia::driver::install(){
   run_file="NVIDIA-Linux-${os_arch}-${version}.run"
   wget http://us.download.nvidia.com/XFree86/Linux-x86_64/${version}/${run_file}
   
+  # Allow x86_32 architecture
+  sudo dpkg --add-architecture i386
+  sudo apt-get update 
+  sudo apt-get install libc6:i386
+
   # Install dependent packages
   sudo apt-get install gcc make
   
   sudo sh ${run_file}
 
 }
+
