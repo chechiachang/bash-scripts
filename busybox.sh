a30a93bdc2d52ae60bc00e4ea321d95cf0457c44
David Chang <chechiachang999@gmail.com>
2018-06-28 17:19:29 +0800

    Add busybox

---
+++ busybox.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cat <<EOF | kubectl create -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: busybox-tool
+  namespace: default
+spec:
+  containers:
+  - image: busybox
+    command:
+      - sleep
+      - "3600"
+    imagePullPolicy: IfNotPresent
+    name: busybox
+  restartPolicy: Always
+EOF
