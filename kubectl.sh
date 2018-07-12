e877ac85136c31d1d4989f004495e64560e81144
David Chang <chechiachang999@gmail.com>
2018-07-12 17:46:22 +0800

    Add create gcr registry secret

--- kubectl.sh
+++ kubectl.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 POD_NAME=$(kubectl get pod -o json | jq -r '.items[] | select( .metadata.name | contains("server-name")) | .metadata.name')
+
+kubectl::secret::registry::create(){
+
+  gcr_user_email=$1
+  gcr_key_file=$2
+  
+  kubectl create secret docker-registry gcr-key \
+    --docker-server=https://asia.gcr.io \
+    --docker-username=_json_key \
+    --docker-email=${gcr_user_email} \
+    --docker-password=$(cat ${gcr_key_file})
+}
