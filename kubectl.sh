ef11467bbf5cca60fee9042e293c94fa6ee6243e
David Chang <chechiachang999@gmail.com>
2018-07-18 16:57:13 +0800

    Add kubectl debug

--- kubectl.sh
+++ kubectl.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+# References
+# - https://kubernetes.io/docs/reference/kubectl/cheatsheet/
 
 POD_NAME=$(kubectl get pod -o json | jq -r '.items[] | select( .metadata.name | contains("server-name")) | .metadata.name')
+
+kubectl::pod::wait_running(){
+
+  pod_name=$1
+
+}
 
 kubectl::secret::registry::create(){
 
@@ -12,4 +20,34 @@ kubectl::secret::registry::create(){
     --docker-username=_json_key \
     --docker-email=${gcr_user_email} \
     --docker-password=$(cat ${gcr_key_file})
+}
+
+kubectl::busybox::apply(){
+
+cat <<EOF | kubectl create -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: busybox-tool
+  namespace: default
+spec:
+  containers:
+  - image: busybox
+    command:
+      - sleep
+      - "3600"
+    imagePullPolicy: IfNotPresent
+    name: busybox
+  restartPolicy: Always
+EOF
+
+}
+
+kubectl::service::debug(){
+  service_name=$1
+
+  kubectl::busybox::apply
+
+  kubectl exec -it busybox-tool nslookup ${service_name}
+
 }
